f410383208cbb83fd41f7850e0c57a62e6334dd6
Samuel Grazan <90648893+Grazan@users.noreply.github.com>
2023-04-20 07:55:44 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,35 +1,24 @@
 var textInput = document.querySelector("#input-texto");
 var outInput = document.querySelector("#output");
 
-
-
-
 function criptografar() {
-
   var texto = textInput.value;
-
-  var resultado = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
-
-
-  document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultado +
-    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+  var resultado = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+  outInput.innerHTML = '<textarea readonly id="input-texto">' + resultado + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar(\'' + resultado + '\')">Copiar</button>';
 }
 
 function descriptografar() {
-
   var texto = textInput.value;
-
   var resultado = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
-
-  document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultado +
-    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+  outInput.innerHTML = '<textarea readonly id="input-texto">' + resultado + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar(\'' + resultado + '\')">Copiar</button>';
 }
 
-function copiar() {
-  var copiar = document.getElementById('input-texto');
-
+function copiar(resultado) {
+  var copiar = document.createElement('textarea');
+  copiar.value = resultado;
+  document.body.appendChild(copiar);
   copiar.select();
   document.execCommand('copy');
+  document.body.removeChild(copiar);
   alert("Texto copiado.");
 }
-
